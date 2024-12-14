8003c3d47b1bf61dcd8edaa138c00b75860a63ae
ChaserHati <catalina.zuniga.y@gmail.com>
2024-12-14 13:44:45 -0300

    vista ultra basica de productos y detalle de producto

---
+++ eGESVEN/Gestor/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+app_name = 'Gestor'
+
+urlpatterns = [
+    path('', views.redirigir_a_productos, name='home'),
+    path('productos/', views.lista_producto, name='lista_producto'),
+    path('productos/<int:pk>/', views.detalle_producto, name='detalle_producto'),
+]
--- eGESVEN/Gestor/views.py
+++ eGESVEN/Gestor/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.shortcuts import redirect
 
+from .models import Producto
 # Create your views here.
+
+def redirigir_a_productos(request):
+    return redirect('Gestor:lista_producto')
+
+def lista_producto(request):
+    productos= Producto.objects.all()
+    return render(request, 'productos/lista_producto.html', {'productos':productos})
+
+def detalle_producto(request, pk):
+    producto = get_object_or_404(Producto, pk=pk)
+    return render(request, 'productos/detalle_producto.html',{'producto':producto})
